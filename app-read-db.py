745827f1d82894a24c3d0f4869f0247f64d74d5f
declercq sebastien <sdq@afnor.fr>
2018-11-06 11:03:24 +0100

    Updating scripts on test branch

--- app-read-db.py
+++ app-read-db.py
@@ -11,18 +11,25 @@ else:
     exit()
 
 
+def name_for_collection_relationship(base, local_cls,
+                                     referred_cls, constraint) -> str:
+    return referred_cls.__name__.lower() + 's'
+
+
 Base = automap_base()
+
+
 engine = create_engine('sqlite:///' + dbname)
-Base.prepare(engine, reflect=True)
+Base.prepare(engine, reflect=True, name_for_collection_relationship=name_for_collection_relationship)
 print(Base.classes.keys())
 session: Session = Session(engine)
 
 r = session.query(Base.classes.Store)
 for store in r:
-    for product in store.product_collection:
+    for product in store.products:
         print('%s is sold at %s' % (product.name, store.name))
 
 r = session.query(Base.classes.Product)
 for product in r:
-    for store in product.store_collection:
+    for store in product.stores:
         print('%s is sold at %s' % (product.name, store.name))
