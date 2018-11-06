f2ee3119c960e8e8e394367fd8ccc329b13809cc
SebDeclercq <declercq_sebastien@yahoo.fr>
2018-11-06 22:06:09 +0100

    DB : insert/update OK + better Exception management in DB.add()

--- db/DB.py
+++ db/DB.py
@@ -19,24 +19,25 @@ class DB:
             self.Store: DeclarativeMeta = db.setup.Store
             self.Category: DeclarativeMeta = db.setup.Category
 
-    def add(self, product: Product) -> bool:
+    def add(self, product: Product) -> int:
+        stores = self._add_stores(product.stores)
+        categories = self._add_categories(product.categories)
         query: Query = self.session.query(self.Product)
         existing_product: Optional[db.setup.Product] = query.filter(
             self.Product.id == product.id
         ).first()
         if existing_product:
-            return True
+            product_id: int = self._update_product(
+                existing_product, product, stores, categories
+            )
         else:
-            stores = self._add_stores(product.stores)
-            categories = self._add_categories(product.categories)
-            self._add_product(product, stores, categories)
-            try:
-                self.session.commit()
-                return True
-            except Exception as err:
-                print(str(err))
-                self.session.rollback()
-                return False
+            product_id = self._add_product(product, stores, categories)
+        try:
+            self.session.commit()
+            return product_id
+        except Exception as err:
+            self.session.rollback()
+            raise err
 
     def _add_product(self, product: Product,
                      stores: List[db.setup.Store],
@@ -83,3 +84,15 @@ class DB:
             else:
                 categories.append(existing_category)
         return categories
+
+    def _update_product(self, existing_product: db.setup.Product,
+                        product: Product, stores: List[db.setup.Store],
+                        categories: List[db.setup.Category]) -> int:
+        existing_product.id = product.id
+        existing_product.name = product.name
+        # existing_product.brands = product.brands
+        existing_product.nutrition_grade = product.nutrition_grades
+        existing_product.url = product.url
+        existing_product.stores = stores
+        existing_product.categories = categories
+        return existing_product.id
