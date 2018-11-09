51f39e8fa85c3c158a26a4e92b1456cdfe12a335
declercq sebastien <sdq@afnor.fr>
2018-11-09 15:41:49 +0100

    Optimized error control in ConsoleUI

--- ui/console/ConsoleUI.py
+++ ui/console/ConsoleUI.py
@@ -9,6 +9,7 @@ from ..UIReturn import UIReturn
 
 class ConsoleUI(UI):
     def __init__(self) -> None:
+        self.error: bool = False
         self.current_level: int = self.WELCOME
         self.page_contents: Dict[int, str] = {}
         self.list_menus: List[int] = [
@@ -19,6 +20,7 @@ class ConsoleUI(UI):
             self.S_PRODUCT_PAGE, self.F_PRODUCT_PAGE, self.S_SAVED_FAVORITE
         ]
         self.history: Any = []
+        self.data: Any = None
         text_file: str = os.path.join('ui', 'console', 'page_contents.yml')
         with open(text_file, encoding='utf-8') as f:
             text: Dict[str, str] = yaml.load(f)
@@ -27,12 +29,20 @@ class ConsoleUI(UI):
             self.page_contents[page_level] = page_content.rstrip()
 
     def display(self, data: Any = None) -> Any:
+        # self._clear_screen()  # Screen gets the hiccups with that command...
+        if self.error:
+            data = self.data
+            self.error = False
+            self.data = None
+            if self.history:
+                self.history.pop()
         if self.current_level == self.WELCOME:
             print(self.page_contents[self.WELCOME])
             self.current_level = self.TOP_MENU
         if self.current_level not in self.product_pages:
             print(self.page_contents[self.current_level])
         if data:
+            self.data = data
             if self.current_level in self.list_menus:
                 actions: Dict[int, int] = {}
                 for idx, element in enumerate(data):
@@ -76,13 +86,19 @@ class ConsoleUI(UI):
                 else:
                     ret.message = self._error(action)
             elif self.current_level == self.S_LIST_CATEGO:
-                self.current_level = self.S_LIST_PRODUCTS
-                ret.action = self.S_LIST_PRODUCTS
-                ret.id_query = self.history[-1][action]
+                if action in self.history[-1]:
+                    self.current_level = self.S_LIST_PRODUCTS
+                    ret.action = self.S_LIST_PRODUCTS
+                    ret.id_query = self.history[-1][action]
+                else:
+                    ret.message = self._error(action)
             elif self.current_level == self.S_LIST_PRODUCTS:
-                self.current_level = self.S_PRODUCT_PAGE
-                ret.action = self.S_PRODUCT_PAGE
-                ret.id_query = self.history[-1][action]
+                if action in self.history[-1]:
+                    self.current_level = self.S_PRODUCT_PAGE
+                    ret.action = self.S_PRODUCT_PAGE
+                    ret.id_query = self.history[-1][action]
+                else:
+                    ret.message = self._error(action)
             elif self.current_level == self.S_PRODUCT_PAGE:
                 if action == 1:
                     self.current_level = self.S_LIST_SUBSTITUTES
@@ -97,19 +113,28 @@ class ConsoleUI(UI):
                             ret.substitution_ids.append(p_id)
                     if len(ret.substitution_ids) != 2:
                         ...
+                else:
+                    ret.message = self._error(action)
             elif self.current_level == self.S_LIST_SUBSTITUTES:
-                self.current_level = self.S_PRODUCT_PAGE
-                ret.action = self.S_PRODUCT_PAGE
-                ret.id_query = self.history[-1][action]
+                if action in self.history[-1]:
+                    self.current_level = self.S_PRODUCT_PAGE
+                    ret.action = self.S_PRODUCT_PAGE
+                    ret.id_query = self.history[-1][action]
+                else:
+                    ret.message = self._error(action)
             elif self.current_level == self.F_LIST_FAVORITES:
-                self.current_level = self.F_PRODUCT_PAGE
-                ret.action = self.F_PRODUCT_PAGE
-                ret.id_query = self.history[-1][action]
+                if action in self.history[-1]:
+                    self.current_level = self.F_PRODUCT_PAGE
+                    ret.action = self.F_PRODUCT_PAGE
+                    ret.id_query = self.history[-1][action]
+                else:
+                    ret.message = self._error(action)
             else:
                 ret.message = self._error(action)
         return ret
 
     def _error(self, action: Union[str, int]) -> str:
+        self.error = True
         message: str = 'Action %s inconnue' % action
         message += '\nVeuillez saisir un chiffre présent dans le menu'
         return message
